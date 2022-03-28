3ffeb2972d8a0f0eacbd4ad94955f81dc8aa685c
Jared Kelly <jpkell046@gmail.com>
2022-03-28 12:42:53 -0700

    to do done

--- cs50/todo/tasks.js
+++ cs50/todo/tasks.js
@@ -1,10 +1,27 @@
 document.addEventListener('DOMContentLoaded', function (){
+
+    //defualt sumbmit button should be disabled
+    submit.disabled = true;
+
+    document.querySelector('#task').onkeyup = () => {
+      if (document.querySelector('#task').vaule.length > 0) {
+        document.querySelector('#sumbit').disabled = false;
+      } else {
+        document.querySelector('#sumbit').disabled = true;
+      }
+     
+    }
+
     document.querySelector('form').onsubmit = () => {
       const task =  document.querySelector('#task').value;
         const li = document.createElement('li');
         li.innerHTML = task;
 
         document.querySelector('#tasks').append(li);
+
+        document.querySelector('#task').value = '';
+
+        submit.disabled = true;
 
         //stop form from sumbitting
         return false;
