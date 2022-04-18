b332b5ddb52a2199e11b7b77285e75301329fcf1
Jared Kelly <jpkell046@gmail.com>
2022-04-18 10:31:30 -0700

    interval and local storage

--- cs50/counter.js
+++ cs50/counter.js
@@ -1,14 +1,17 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+ localStorage.setItem('counter', 0);
+}
+
 function count() {
+    let counter =localStorage.getItem('counter');
     counter ++;
     document.querySelector('h1').innerHTML = counter;
-
-    if (counter % 10 === 0) {
-        alert (`counter is now ${counter}`)
-    }
+    localStorage.setItem('counter', counter);
 }
+
 //to use event listenres the first is what it is waiting  for the second argument tell what yo run
 document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 });
 
